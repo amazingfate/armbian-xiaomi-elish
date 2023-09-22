d18b32a1678cd209ef65226059241d116f449002
amazingfate <liujianfeng1994@gmail.com>
2023-09-22 12:58:00 +0800

    not build with esp partition

--- create-seperated-image.sh
+++ create-seperated-image.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 set -ex
 image_name=$1
-efi_start_sector=$(gdisk -l ./${image_name}|grep efi|awk '{print $2}')
-efi_end_sector=$(gdisk -l ./${image_name}|grep efi|awk '{print $3}')
 rootfs_start_sector=$(gdisk -l ./${image_name}|grep rootfs|awk '{print $2}')
 rootfs_end_sector=$(gdisk -l ./${image_name}|grep rootfs|awk '{print $3}')
-rm esp*.img rootfs*.img ||true
-dd if=./${image_name} skip=${efi_start_sector} count=$((${efi_end_sector} - ${efi_start_sector})) of=esp.img
+rm rootfs*.img ||true
 dd if=./${image_name} skip=${rootfs_start_sector} count=$((${rootfs_end_sector} - ${rootfs_start_sector})) of=rootfs.img
 rm ${image_name}
 
 old_rootfs_image=rootfs.img
 old_rootfs_image_mount_dir=rootfs
 old_rootfs_image_uuid=$(blkid -s UUID -o value ${old_rootfs_image})
-old_esp_image=esp.img
-old_esp_image_mount_dir=esp
-old_esp_image_uuid=$(blkid -s UUID -o value ${old_esp_image})
 new_rootfs_image=${image_name%*.img}-rootfs.img
 new_rootfs_image_mount_dir=rootfs-new
-new_esp_image=${image_name%*.img}-esp.img
-new_esp_image_mount_dir=esp-new
-mkdir -p ${old_rootfs_image_mount_dir} ${old_esp_image_mount_dir} ${new_rootfs_image_mount_dir} ${new_esp_image_mount_dir}
+mkdir -p ${old_rootfs_image_mount_dir} ${new_rootfs_image_mount_dir}
 truncate --size=8192M ${new_rootfs_image}
-truncate --size=200M ${new_esp_image}
-mkfs.fat -S 4096 ${new_esp_image}
 mkfs.ext4 -F ${new_rootfs_image}
-new_esp_image_uuid=$(blkid -s UUID -o value ${new_esp_image})
 new_rootfs_image_uuid=$(blkid -s UUID -o value ${new_rootfs_image})
-sudo mount ${old_esp_image} ${old_esp_image_mount_dir}
-sudo mount ${new_esp_image} ${new_esp_image_mount_dir}
-sudo cp -rfp ${old_esp_image_mount_dir}/* ${new_esp_image_mount_dir}/
-sudo umount ${new_esp_image_mount_dir}
-sudo umount ${old_esp_image_mount_dir}
 sudo mount ${old_rootfs_image} ${old_rootfs_image_mount_dir}
 sudo mount ${new_rootfs_image} ${new_rootfs_image_mount_dir}
 sudo cp -rfp ${old_rootfs_image_mount_dir}/* ${new_rootfs_image_mount_dir}/
 sudo sed -i "s|${old_rootfs_image_uuid}|${new_rootfs_image_uuid}|g" ${new_rootfs_image_mount_dir}/etc/fstab
-sudo sed -i "s|${old_esp_image_uuid}|${new_esp_image_uuid}|g" ${new_rootfs_image_mount_dir}/etc/fstab
 gzip -c ./${new_rootfs_image_mount_dir}/boot/vmlinuz-*-sm8250-arm64 > Image.gz
 for panel_type in boe csot
 do
@@ -58,7 +41,6 @@ sudo umount ${old_rootfs_image_mount_dir}
 e2fsck -p -f ${new_rootfs_image}
 resize2fs -M ${new_rootfs_image}
 xz -z -T0 ${new_rootfs_image}
-xz -z -T0 ${new_esp_image}
 xz -z -T0 armbian-kernel-boe.img
 xz -z -T0 armbian-kernel-csot.img
-rm ${old_rootfs_image} ${old_esp_image} ||true
+rm ${old_rootfs_image} ||true
